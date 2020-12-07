ef21130156d1e284c99c6a7408111b0c15a9ea5a
Ng Liang Ming <ngliangming@gmail.com>
2020-12-07 18:43:06 +0800

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -32,9 +32,9 @@
         </div>
 
     </main>
-            <p>
-                Here at Aeki Company, we sell an assortment of furniture ranging from tables to bed mattresses. We pride ourselves on our quality furniture and customer service. We hope you'll be able to find that our wide range of selection suits your taste.
-            </p>
+            <p>s
+                Here at Aeki a, we sell an assortment of furniture ranging from tables to bed mattresses. We pride ourselves on our quality furniture and customer service. We hope you'll be able to find that our wide range of selection suits your taste.
+            </p>s
     <!--//Main ends here -->
 test
 <?php include 'includes/footer.html'?>
