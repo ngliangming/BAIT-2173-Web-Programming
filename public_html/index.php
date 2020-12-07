c6e110069c86ec2af23575b23d7750a74e4a5614
ngliangming <69562689+ngliangming@users.noreply.github.com>
2020-12-07 18:18:04 +0800

    update to index via browser

--- public_html/index.php
+++ public_html/index.php
@@ -31,10 +31,7 @@
             <p>
                 Here at Aeki Company, we sell an assortment of furniture ranging from tables to bed mattresses. We pride ourselves on our quality furniture and customer service. We hope you'll be able to find that our wide range of selection suits your taste.
             </p>
-            
-            <p>
-                This is from desktop! I have changed!
-            </p>
+            I'll try this i guess
             
             <a href="products.php" class="btn btn-lg btn-secondary cust-btn">Proceed to store</a>
         </div>
