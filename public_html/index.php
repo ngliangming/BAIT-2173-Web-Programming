dcf7face3584cec6236c1a3b27576c7edae2c332
Ng Liang Ming <ngliangming@gmail.com>
2020-12-09 11:26:17 +0800

    Revert "Update index.php"
    This reverts commit 9a4951139da698b8fc8ba7ab2386eac5ad613813.

--- public_html/index.php
+++ public_html/index.php
@@ -37,9 +37,6 @@
             <a href="products.php" class="btn btn-lg btn-secondary cust-btn">Proceed to store</a>
         </div>
 
-            <p>
-                Here at Aeki Company, we sell an assortment of furniture ranging from tables to bed mattresses. We pride ourselves on our quality furniture and customer service. We hope you'll be able to find that our wide range of selection suits your taste.
-            </p>
     </main>
     <!--//Main ends here -->
 
